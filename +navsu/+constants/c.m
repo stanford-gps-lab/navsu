e0a4235d5c744d608d4786533116554fcd50c47c
Kazuma Gunning <kgunning@stanford.edu>
2020-04-15 16:46:59 -0700

    New constant functions

---
+++ +navsu/+constants/c.m
@@ -0,0 +1,14 @@
+function val = c
+% c
+% DESCRIPTION:
+%  This function just stores the speed of light as a constant. 
+% INPUT:
+%   None
+% OUTPUT:
+%   val     - the speed of light (m/s)
+%
+% See also: 
+
+val =  299792458; % speed of light (m/s)
+
+end
---
+++ +navsu/+constants/rEarth.m
@@ -0,0 +1,14 @@
+function val = rEarth
+% rEarth
+% DESCRIPTION:
+%  This function just stores the mean Earth radius as a constant. 
+% INPUT:
+%   None
+% OUTPUT:
+%   val     - Mean Earth radius in m
+%
+% See also: 
+
+val = 6371.008e3;
+
+end
