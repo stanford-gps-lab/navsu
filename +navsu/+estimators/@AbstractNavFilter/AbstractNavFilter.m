7cdb59671f1b55aaf8ba4dc6389f6c22864a2800
kgunning <kazgunning@gmail.com>
2020-05-17 13:24:18 -0700

    Moved things out of the abstract navigation filter class

--- +navsu/+estimators/@AbstractNavFilter/AbstractNavFilter.m
+++ +navsu/+estimators/@AbstractNavFilter/AbstractNavFilter.m
@@ -1,27 +1,9 @@
 classdef (Abstract) AbstractNavFilter < matlab.mixin.Copyable 
     
     properties
-        state   % the state of the filter 
-        cov     % covariance of the state
-        
-        pos                % ECEF position
-        vel                % ECEF velocity
-        R_b_e              % DCM from body to ECEF
-        imuBiasStates      % imu bias states
-        clockBias          % receiver clock bias(es)
-        clockDrift         % receiever clock drift(s)
-        carrierAmbiguities % carrier phase ambiguity estimates
-        
-        INDS_STATE % state indexing
-        
         initialized = 0 % whether or not the filter has been initialized.
         % 0 = not started. 1 = initialized but that's it. 2 = initialized
         % with at least one time and measurement update
-        
-        PARAMS % parameters associated with the running of this filter!
-        
-         % phase windup
-        phWind = struct('phaseOffset',zeros(0,1),'PrnConstInd',zeros(0,2));
         
     end
     
@@ -39,52 +21,6 @@ classdef (Abstract) AbstractNavFilter < matlab.mixin.Copyable
         outData = saveState(obj,outData,epoch,obs);
     end    
     
-    methods
-        % These classes have already been implemented and are mostly just
-        % useful for the various GNSS estimators
-        
-        
-        % Build predicted measurements, sensitivity matrix, and pull
-        % measurement IDs for GNSS measurements
-        [predMeas,H,R,el,az,prnConstInds,idList,measList,measIdRemovedLow,extraInputs] =  ...
-            handleGnssMeas(obj,epoch,obs,corrData,varargin)
-        
-        % doppModel- called within handleGnssMeas and produces predicted
-        % value and sensitivity matrix for doppler measurements
-        [predMeas,H,sig] = doppModel(obj,nState,dVel,A,rxDrift,constInd)
-        
-        % carrierModel- called within handleGnssMeas and produces predicted
-        % value and sensitivity matrix for carrier phase measurements
-        [predMeas,H,sig] = carrierModel(obj,nState,sigi,freqi,tecSlant,state,m,indIonosi, ...
-            indMpCarrsi,indAmbStatesi,phWind,gRange,satBias,rxBias,trop,stRangeOffset,...
-            relClockCorr,relRangeCorr,A,constIndi)
-        
-        % codeModel- called within handleGnssMeas and produces predicted
-        % value and sensitivity matrix for code phase measurements
-        [predMeas,H,sig] = codeModel(obj,SimpleModel,nState,sigi,freqi,tecSlant,state,...
-            constIndi,indGloDcbsi,indMpCodesi,m,gRange,satBias,rxBias,trop,stRangeOffset,...
-            relClockCorr,relRangeCorr,A,indIonosi)
-        
-        % handleVehicleConstraintPseudomeas- produce predicted value and
-        % sensitivity matrix for vehicle slip constraints
-        [pred_measi,Hi,ri,measIdi,measi] = handleVehicleConstraintPseudomeas(obj)
-        
-        % handlePositionMeas- produce predicted value and
-        % sensitivity matrix for direct position measurements
-        [predMeasi,Hi,Ri,measIdi,measi] = handlePositionMeas(obj,posMeas)
-        
-        % handleVelocityMeas- produce predicted value and
-        % sensitivity matrix for direct velocity measurements
-        [predMeasi,Hi,Ri,measIdi,measi] = handleVelocityMeas(obj,velMeas)
-        
-        % leastSquaresSol - produce a least squares solution based on the 
-        % available observations.  This currently requires GNSS
-        % observations
-        [complete, measId,extraInputs] = leastSquaresSol(obj,epoch,obs,corrData,varargin)
-
-        [posApc,velApc] = posVelApc(obj);  % Position and velocity of the GNSS antenna phase center.
-
-    end
     
 end
     
--- +navsu/+estimators/@pppFilter/pppFilter.m
+++ +navsu/+estimators/@pppFilter/pppFilter.m
@@ -2,6 +2,25 @@ classdef pppFilter < navsu.estimators.AbstractNavFilter
     
     
     properties
+        state   % the state of the filter 
+        cov     % covariance of the state
+        
+        pos                % ECEF position
+        vel                % ECEF velocity
+        R_b_e              % DCM from body to ECEF
+        imuBiasStates      % imu bias states
+        clockBias          % receiver clock bias(es)
+        clockDrift         % receiever clock drift(s)
+        carrierAmbiguities % carrier phase ambiguity estimates
+        
+        INDS_STATE % state indexing
+        
+        
+        PARAMS % parameters associated with the running of this filter!
+        
+         % phase windup
+        phWind = struct('phaseOffset',zeros(0,1),'PrnConstInd',zeros(0,2));
+        
         
         lastAccMeas % most recent acceleration measurements
         lastGyroMeas % most recent gyro measurement
@@ -57,6 +76,54 @@ classdef pppFilter < navsu.estimators.AbstractNavFilter
         
         outData = saveState(obj,outData,epoch,obs);
                
+    end
+    
+    
+    methods
+        % These classes have already been implemented and are mostly just
+        % useful for the various GNSS estimators, including least squares
+        % and inertial PPP filters
+        
+        % Build predicted measurements, sensitivity matrix, and pull
+        % measurement IDs for GNSS measurements
+        [predMeas,H,R,el,az,prnConstInds,idList,measList,measIdRemovedLow,extraInputs] =  ...
+            handleGnssMeas(obj,epoch,obs,corrData,varargin)
+        
+        % doppModel- called within handleGnssMeas and produces predicted
+        % value and sensitivity matrix for doppler measurements
+        [predMeas,H,sig] = doppModel(obj,nState,dVel,A,rxDrift,constInd)
+        
+        % carrierModel- called within handleGnssMeas and produces predicted
+        % value and sensitivity matrix for carrier phase measurements
+        [predMeas,H,sig] = carrierModel(obj,nState,sigi,freqi,tecSlant,state,m,indIonosi, ...
+            indMpCarrsi,indAmbStatesi,phWind,gRange,satBias,rxBias,trop,stRangeOffset,...
+            relClockCorr,relRangeCorr,A,constIndi)
+        
+        % codeModel- called within handleGnssMeas and produces predicted
+        % value and sensitivity matrix for code phase measurements
+        [predMeas,H,sig] = codeModel(obj,SimpleModel,nState,sigi,freqi,tecSlant,state,...
+            constIndi,indGloDcbsi,indMpCodesi,m,gRange,satBias,rxBias,trop,stRangeOffset,...
+            relClockCorr,relRangeCorr,A,indIonosi)
+        
+        % handleVehicleConstraintPseudomeas- produce predicted value and
+        % sensitivity matrix for vehicle slip constraints
+        [pred_measi,Hi,ri,measIdi,measi] = handleVehicleConstraintPseudomeas(obj)
+        
+        % handlePositionMeas- produce predicted value and
+        % sensitivity matrix for direct position measurements
+        [predMeasi,Hi,Ri,measIdi,measi] = handlePositionMeas(obj,posMeas)
+        
+        % handleVelocityMeas- produce predicted value and
+        % sensitivity matrix for direct velocity measurements
+        [predMeasi,Hi,Ri,measIdi,measi] = handleVelocityMeas(obj,velMeas)
+        
+        % leastSquaresSol - produce a least squares solution based on the 
+        % available observations.  This currently requires GNSS
+        % observations
+        [complete, measId,extraInputs] = leastSquaresSol(obj,epoch,obs,corrData,varargin)
+
+        [posApc,velApc] = posVelApc(obj);  % Position and velocity of the GNSS antenna phase center.
+
     end
     
     methods %(Access = private)
