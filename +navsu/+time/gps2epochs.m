deebda27c12ad30459255097dcb5c5c9f42dc379
AMM <amm.technical.consulting@gmail.com>
2020-07-18 08:32:25 -0700

    Handle input when specified as a single Nx2 matrix [weekNum TOW] in addition to the original two-argument input format. Should be backwards compatible with earlier (two-argument only) implementation.

--- +navsu/+time/gps2epochs.m
+++ +navsu/+time/gps2epochs.m
@@ -6,11 +6,18 @@ function epochs = gps2epochs(gpsWeek,tow)
 % INPUT:
 %   gpsWeek  = GPS week with no rollover [Nx1]
 %   gpsTow   = GPS time of week [Nx1]
+%    -- or --
+%   [gpsWeek gpsTow] = same as above, only in a single matrix of size [Nx2]
 %
 % OUTPUT:
 %   epochs = GPS epoch [Nx1]
 %
 % See also: navsu.time.epochs2gps
+
+if nargin == 1 && size(gpsWeek, 2) == 2, % input is [weekNum TOW] (size N x 2)
+    tow = gpsWeek(:, 2);
+    gpsWeek = gpsWeek(:, 1);
+end
 
 epochs = gpsWeek*604800+tow;
 
