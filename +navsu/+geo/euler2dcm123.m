0d94a6495570035ad4bd3f97ac637fab257d99ff
Fabian <fabianr@stanford.edu>
2021-08-09 11:58:34 +0200

    typo bugfix in geo.euler2dcm123 rotation matrix

--- +navsu/+geo/euler2dcm123.m
+++ +navsu/+geo/euler2dcm123.m
@@ -12,7 +12,7 @@ psi   = eulers(3);
 %     cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi) cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi) cos(theta)*cos(phi)];
 
 dcm = [cos(theta)*cos(psi)                               cos(theta)*sin(psi)                        -sin(theta);
-    sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi) sin(psi)*sin(theta)*sin(psi)+cos(phi)*cos(psi) cos(theta)*sin(phi);
+    sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi) sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi) cos(theta)*sin(phi);
     cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi) cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi) cos(theta)*cos(phi)];
 
 
