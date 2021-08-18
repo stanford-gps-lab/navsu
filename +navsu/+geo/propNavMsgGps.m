fe652eba0e6de8d4d51ed9d1e1110647be50e324
Fabian <fabianr@stanford.edu>
2021-08-18 12:51:33 +0200

    vectorized several GPS orbit propagation steps in propNavMsgGps

--- +navsu/+geo/propNavMsgGps.m
+++ +navsu/+geo/propNavMsgGps.m
@@ -71,7 +71,9 @@ if strcmp(constellation,'GAL')
 
 end
 
-prnLast = 0;
+[iEph, iLastU] = deal(zeros(tmArrayLen, 1));
+
+% prnLast = 0;
 for loop = 1:tmArrayLen
     % find the most recent prior ephemeris
 %     if prnLast ~= prn(loop)
@@ -121,11 +123,11 @@ for loop = 1:tmArrayLen
                 tmp = tmArray(loop) - eph.GPS_week_num(jdx) * 604800 - eph.TTOM(jdx);
                 tmp(tmp < 0) = max(tmp) - tmp(tmp < 0);
                 %     t(t < 60) = max(t) - t(t < 60);
-                [~, K] = min(tmp(end:-1:1));
-                K = jdx(length(tmp) + 1 - K);
+                [~, iLastUpload] = min(tmp(end:-1:1));
+                iLastUpload = jdx(length(tmp) + 1 - iLastUpload);
             else
                 [~, tmp] = min(eph.GPS_week_num(idx) * 604800 + eph.TTOM(idx));
-                K = idx(tmp);
+                iLastUpload = idx(tmp);
             end            
             
         case 'GAL'
@@ -149,138 +151,160 @@ for loop = 1:tmArrayLen
         fprintf(2, 'Warning: SV health %g for the ephemeris No.%d\n', eph.health(I), I);
     end
     
-    A = eph.sqrtA(I)^2; % semimajor axis  
-    n0 = sqrt(mu / A^3); % mean motion in rad/s
-    if strcmp(constellation,'BDS')
-        % need to adjust for Beidou timescale leapseconds
-        t = tmArray(loop) - eph.GPS_week_num(I) * 604800 - eph.Toe(I)-14; % time from ephemeris reference epoch
-    else
-        t = tmArray(loop) - eph.GPS_week_num(I) * 604800 - eph.Toe(I); % time from ephemeris reference epoch
-    end
-%     t = mod(t + 302400, 604800) - 302400; % !!
-    if isfield(eph,'aDot')
-        A = A + eph.aDot(I)*t; % correct A with Adot if CNAV
-    end
+    % from this point on we need the following indices:
+    % loop  - index of satellite / epoch to propagate 1:N
+    % I     - index of ephemeris struct item to use for propagation
+    % iLastUpload - index of ephemeris struct of last upload
+    iEph(loop) = I;
+    iLastU(loop) = iLastUpload;
+    
+end
 
+% the remaining calculations can be done vectorized
 
-    % check fit interval
-    if WARNING_ENABLE && t > eph.Fit_interval(I) * 3600
-        fprintf(2, 'Warning: t = %g (second) exceeds the Fit Interval %g (hour)\n', t, eph.Fit_interval(I));
+% time since ephemeris for each SV
+tsE = tmArray - eph.GPS_week_num(iEph) * 604800 - eph.Toe(iEph);
+if strcmp(constellation,'BDS')
+    tsE = tsE - 14; % need to adjust for Beidou timescale leapseconds
+end
+
+% check fit interval
+if WARNING_ENABLE 
+    for iWarn = find(tsE > eph.Fit_interval(iEph) * 3600)'
+        fprintf(2, 'Warning: t = %g (second) exceeds the Fit Interval %g (hour)\n', ...
+            tsE(iWarn), eph.Fit_interval(iWarn));
     end
-    
-    n = n0 + eph.Delta_n(I); % corrected mean motion
-    M = eph.M0(I) + n * t; % mean anomaly
-    e = eph.e(I); % eccentricity
-    E = KeplerEq(M, e); % solve Kepler's eq for eccentric anomaly
-    nu = atan2(sqrt(1 - e.^2) * sin(E), cos(E) - e); % true anomaly
-    PHI = nu + eph.omega(I); % argument of latitude
-    
-    % correct argument of latitude, radius, and inclination
-    d_uri = [eph.Cus(I) eph.Cuc(I); eph.Crs(I) eph.Crc(I); eph.Cis(I) eph.Cic(I)] ...
-        * [sin(2*PHI); cos(2*PHI)];
-    uri = d_uri + [PHI; A .* (1 - e .* cos(E)); eph.i0(I) + eph.I_DOT(I)*t];
-    
-    xo = uri(2) .* cos(uri(1)); % SV x-position in orbital plane
-    yo = uri(2) .* sin(uri(1)); % SV y-position in orbital plane
-    
-    % corrected longitude of ascending node
-     if strcmp(constellation,'BDS') && prn(loop) <= 5
-        OMEGA = eph.OMEGA(I) + (eph.OMEGA_DOT(I)) * t - OMEGA_DOTe * eph.Toe(I);
-        
-        % compute SV position in ECEF
-        pos.x(loop) = xo * cos(OMEGA) - yo * cos(uri(3)) * sin(OMEGA);
-        pos.y(loop) = xo * sin(OMEGA) + yo * cos(uri(3)) * cos(OMEGA);
-        pos.z(loop) = yo * sin(uri(3));
-        % need to further rotate Beidou GEOs
-        phiTemp = -5*pi/180;
-        RxTemp = [1 0 0; 0 cos(phiTemp) sin(phiTemp); 0 -sin(phiTemp) cos(phiTemp)];
-        phiTemp = OMEGA_DOTe*t;
+end
+
+%% compute SV position
+
+A = eph.sqrtA(iEph).^2; % semimajor axis  
+n0 = sqrt(mu ./ A.^3); % mean motion in rad/s
+
+if isfield(eph,'aDot')
+    A = A + eph.aDot(iEph) .* tsE; % correct A with Adot if CNAV
+end
+
+n = n0 + eph.Delta_n(iEph); % corrected mean motion
+M = eph.M0(iEph) + n .* tsE; % mean anomaly
+e = eph.e(iEph); % eccentricity
+E = KeplerEq(M, e); % solve Kepler's eq for eccentric anomaly
+nu = atan2(sqrt(1 - e.^2) .* sin(E), cos(E) - e); % true anomaly
+PHI = nu + eph.omega(iEph); % argument of latitude
+
+% correct argument of latitude, radius, and inclination      
+d_uri = [dot([eph.Cus(iEph) eph.Cuc(iEph)], [sin(2*PHI) cos(2*PHI)], 2) ...
+         dot([eph.Crs(iEph) eph.Crc(iEph)], [sin(2*PHI) cos(2*PHI)], 2) ...
+         dot([eph.Cis(iEph) eph.Cic(iEph)], [sin(2*PHI) cos(2*PHI)], 2)];
+
+uri = d_uri + [PHI, A .* (1 - e .* cos(E)), eph.i0(iEph) + eph.I_DOT(iEph).*tsE];
+
+xo = uri(:, 2) .* cos(uri(:, 1)); % SV x-position in orbital plane
+yo = uri(:, 2) .* sin(uri(:, 1)); % SV y-position in orbital plane
+
+% corrected longitude of ascending node for some BDS satellites
+
+OMEGA = eph.OMEGA(iEph) ...
+      + (eph.OMEGA_DOT(iEph) - OMEGA_DOTe) .* tsE...
+      - OMEGA_DOTe * eph.Toe(iEph);
+
+if strcmp(constellation, 'BDS')
+    b2c = prn <= 5;
+    OMEGA(b2c) = OMEGA(b2c) - eph.OMEGA_DOT(iEph(b2c)).*tsE(b2c);
+end
+
+% compute SV position in ECEF
+pos.x = xo .* cos(OMEGA) - yo .* cos(uri(:, 3)) .* sin(OMEGA);
+pos.y = xo .* sin(OMEGA) + yo .* cos(uri(:, 3)) .* cos(OMEGA);
+pos.z = yo .* sin(uri(:, 3));
+
+if strcmp(constellation, 'BDS') && any(b2c)
+    % need to further rotate Beidou GEOs
+    phiTemp = -5*pi/180;
+    RxTemp = [1 0 0; 0 cos(phiTemp) sin(phiTemp); 0 -sin(phiTemp) cos(phiTemp)];
+    for i2c = find(b2c)'
+        phiTemp = OMEGA_DOTe*tsE(i2c);
         RzTemp = [cos(phiTemp) sin(phiTemp) 0; -sin(phiTemp) cos(phiTemp) 0; 0 0 1];
-        
-        posTemp = RzTemp*RxTemp*[pos.x(loop) pos.y(loop) pos.z(loop)]';
-        pos.x(loop) = posTemp(1);
-        pos.y(loop) = posTemp(2);
-        pos.z(loop) = posTemp(3);
-    else
-        OMEGA = eph.OMEGA(I) + (eph.OMEGA_DOT(I) - OMEGA_DOTe) * t - OMEGA_DOTe * eph.Toe(I);
-        
-        % compute SV position in ECEF
-        pos.x(loop) = xo * cos(OMEGA) - yo * cos(uri(3)) * sin(OMEGA);
-        pos.y(loop) = xo * sin(OMEGA) + yo * cos(uri(3)) * cos(OMEGA);
-        pos.z(loop) = yo * sin(uri(3));
-    end
 
-    
-    M_dot = n; % rate of mean anomaly
-    E_dot = M_dot ./ (1 - e .* cos(E)); % rate of eccentric anomaly
-    nu_dot = E_dot .* sqrt(1 - e.^2) ./ (1 - e .* cos(E)); % rate of true anomaly
-    PHI_dot = nu_dot; % rate of argument of latitude
+        posTemp = RzTemp*RxTemp*[pos.x(i2c) pos.y(i2c) pos.z(i2c)]';
+        pos.x(i2c) = posTemp(1);
+        pos.y(i2c) = posTemp(2);
+        pos.z(i2c) = posTemp(3);
+    end
+end
 
-    % rates of argument of latitude, radius, and inclination
-    d_uri_dot = ([eph.Cus(I) eph.Cuc(I); eph.Crs(I) eph.Crc(I); ...
-                   eph.Cis(I) eph.Cic(I)] ...
-                 * [cos(2*PHI); -sin(2*PHI)])*(2*PHI_dot);
-    uri_dot = d_uri_dot + [PHI_dot; A .* e .* sin(E) .* E_dot; eph.I_DOT(I)];
-           
-    % SV x-velocity in orbital plane
-    xo_dot = uri_dot(2) .* cos(uri(1)) - uri(2) .* sin(uri(1)) .* uri_dot(1); 
-    % SV y-velocity in orbital plane
-    yo_dot = uri_dot(2) .* sin(uri(1)) + uri(2) .* cos(uri(1)) .* uri_dot(1); 
-    % rate of longitude of ascending node
-    OMEGA_dot = eph.OMEGA_DOT(I) - OMEGA_DOTe;   
-    
-    % compute SV velocity in ECEF
-    pos.x_dot(loop) = xo_dot * cos(OMEGA) - xo * sin(OMEGA) * OMEGA_dot ...
-                      - yo_dot * cos(uri(3)) * sin(OMEGA) ...
-                      + yo * sin(uri(3)) * sin(OMEGA) * uri_dot(3) ...
-                      - yo * cos(uri(3)) * cos(OMEGA) * OMEGA_dot;
-    pos.y_dot(loop) = xo_dot * sin(OMEGA) + xo * cos(OMEGA) * OMEGA_dot ...
-                      + yo_dot * cos(uri(3)) * cos(OMEGA) ...
-                      - yo * sin(uri(3)) * cos(OMEGA) * uri_dot(3) ...
-                      - yo * cos(uri(3)) * sin(OMEGA) * OMEGA_dot;                      
-    pos.z_dot(loop) = yo_dot * sin(uri(3)) + yo * cos(uri(3)) * uri_dot(3);    
+%% compute SV velocity
+M_dot = n; % rate of mean anomaly
+E_dot = M_dot ./ (1 - e .* cos(E)); % rate of eccentric anomaly
+nu_dot = E_dot .* sqrt(1 - e.^2) ./ (1 - e .* cos(E)); % rate of true anomaly
+PHI_dot = nu_dot; % rate of argument of latitude
 
-    % compute SV clock bias and drift
-    t = tmArray(loop) - eph.GPS_week_num(I) * 604800 - eph.Toc(I); % time from clock reference epoch
-    pos.clock_drift(loop) = eph.clock_drift(I) + eph.clock_drift_rate(I) * t;
-    
-    if dualFreq 
-        % Beidou dual frequency TGD offsets
-        kf = (1561.098/1207.14)^2;
-        tgdOffset = (eph.TGD2(I)-kf*eph.TGD(I))./(1-kf);
-        pos.clock_bias(loop) = eph.clock_bias(I) + pos.clock_drift(loop) * t-tgdOffset;
-    else
-        pos.clock_bias(loop) = eph.clock_bias(I) + pos.clock_drift(loop) * t;
-    end
-    % pos.clock_bias(loop) = eph.clock_bias(I) + pos.clock_drift(loop) * t - 4.442807633e-10 * e * eph.sqrtA(I) * sin(E);
-    
-    % Relativisitic effect
-    pos.clock_rel(loop) = -4.442807633e-10*e*eph.sqrtA(I)*sin(E);
-    if dualFreq
-        pos.TGD(loop) = tgdOffset;
-    else
-        pos.TGD(loop) = eph.TGD(I);
-    end
-    pos.t_m_toc(loop) = t;
-    pos.accuracy(loop) = eph.accuracy(I);
-    pos.health(loop) = eph.health(I);
-    pos.IODC(loop) = eph.IODC(I);
-    pos.t_m_toe(loop) = tmArray(loop) - eph.GPS_week_num(I) * 604800 - eph.Toe(I);
-    pos.AoD(loop) = tmArray(loop) - eph.GPS_week_num(I) * 604800 - eph.Toe(I);
-    
-    switch constellation 
-        case 'GPS'
-            pos.tslu(loop) = tmArray(loop) - eph.GPS_week_num(K) * 604800 - eph.TTOM(K);
-            
-        case 'BDS'
-            pos.tslu(loop) = aodi;
-    end
-    pos.toe_m_ttom(loop) = eph.Toe(I) - eph.TTOM(I);
-    pos.Fit_interval(loop) = eph.Fit_interval(I);
+% rates of argument of latitude, radius, and inclination             
+d_uri_dot = [dot([eph.Cus(iEph) eph.Cuc(iEph)], [cos(2*PHI), -sin(2*PHI)], 2) ...
+             dot([eph.Crs(iEph) eph.Crc(iEph)], [cos(2*PHI), -sin(2*PHI)], 2) ...
+             dot([eph.Cis(iEph) eph.Cic(iEph)], [cos(2*PHI), -sin(2*PHI)], 2)] ...
+           .* (2*PHI_dot);
+
+uri_dot = d_uri_dot + [PHI_dot, A .* e .* sin(E) .* E_dot, eph.I_DOT(iEph)];
+
+% SV x-velocity in orbital plane
+xo_dot = uri_dot(:, 2) .* cos(uri(:, 1)) - uri(:, 2) .* sin(uri(:, 1)) .* uri_dot(:, 1); 
+% SV y-velocity in orbital plane
+yo_dot = uri_dot(:, 2) .* sin(uri(:, 1)) + uri(:, 2) .* cos(uri(:, 1)) .* uri_dot(:, 1); 
+% rate of longitude of ascending node
+OMEGA_dot = eph.OMEGA_DOT(iEph) - OMEGA_DOTe;   
+
+% compute SV velocity in ECEF
+pos.x_dot = xo_dot .* cos(OMEGA) - xo .* sin(OMEGA) .* OMEGA_dot ...
+          - yo_dot .* cos(uri(:, 3)) .* sin(OMEGA) ...
+          + yo .* sin(uri(:, 3)) .* sin(OMEGA) .* uri_dot(:, 3) ...
+          - yo .* cos(uri(:, 3)) .* cos(OMEGA) .* OMEGA_dot;
+pos.y_dot = xo_dot .* sin(OMEGA) + xo .* cos(OMEGA) .* OMEGA_dot ...
+          + yo_dot .* cos(uri(:, 3)) .* cos(OMEGA) ...
+          - yo .* sin(uri(:, 3)) .* cos(OMEGA) .* uri_dot(:, 3) ...
+          - yo .* cos(uri(:, 3)) .* sin(OMEGA) .* OMEGA_dot;                      
+pos.z_dot = yo_dot .* sin(uri(:, 3)) + yo .* cos(uri(:, 3)) .* uri_dot(:, 3);    
+
+%% compute SV clock bias and drift
+toC = tmArray - eph.GPS_week_num(iEph) * 604800 - eph.Toc(iEph); % time from clock reference epoch
+pos.clock_drift = eph.clock_drift(iEph) + eph.clock_drift_rate(iEph) .* toC;
+
+pos.clock_bias = eph.clock_bias(iEph) + pos.clock_drift .* toC;
+
+if dualFreq 
+    % Beidou dual frequency TGD offsets
+    kf = (1561.098/1207.14)^2;
+    tgdOffset = (eph.TGD2(iEph)-kf*eph.TGD(iEph)) ./ (1-kf);
+    pos.clock_bias = pos.clock_bias - tgdOffset;
+end
+
+%% Relativisitic effect, other elements
+pos.clock_rel = -4.442807633e-10 * e .* eph.sqrtA(iEph) .* sin(E);
+if dualFreq
+    pos.TGD = tgdOffset;
+else
+    pos.TGD = eph.TGD(iEph);
+end
+pos.t_m_toc     = toC;
+pos.accuracy    = eph.accuracy(iEph);
+pos.health      = eph.health(iEph);
+pos.IODC        = eph.IODC(iEph);
+pos.t_m_toe     = tsE;
+pos.AoD         = tmArray - eph.GPS_week_num(iEph) * 604800 - eph.Toe(iEph); % what is this??
+
+switch constellation 
+    case 'GPS'
+        pos.tslu = tmArray - eph.GPS_week_num(iLastU) * 604800 - eph.TTOM(iLastU);
+
+    case 'BDS'
+        pos.tslu = aodi;
+end
+pos.toe_m_ttom  = eph.Toe(iEph) - eph.TTOM(iEph);
+pos.Fit_interval = eph.Fit_interval(iEph);
     
 end
 
-end
+% end
 
 function E = KeplerEq(M0, e)
 % E = KeplerEq(M, e);
@@ -308,14 +332,16 @@ tol = 1e-15;      % convergence criterion
 max_iter = 10;    % max iteration 
 
 m = 1;            % iteration counter initial value
-dE = 1.0;         % set initial value of delta-E to fail convergenge
+dE = ones(size(e));         % set initial value of delta-E to fail convergence
+nConv = abs(dE) > tol; % index of satellites that have not yet converged
 
 E = M + (e .* sin(M)) ./ (1 - sin(M + e) + sin(M));    % set initial value of E 
 
-while any(abs(dE) > tol) && (m <= max_iter)
+while any(nConv) && (m <= max_iter)
   dE = (E - e .* sin(E) - M) ./ (1 - e .* cos(E));
-  E = E - dE;         % update E
+  E(nConv) = E(nConv) - dE(nConv);         % update E
   m = m + 1;          % increment iteration numbers
+  nConv = abs(dE) > tol; % check again for satellites that have not converged
 
   if m > max_iter     % check against maximum iterations allowed
     fprintf(2, 'Warning message from KEPLR_EQ ...\n')
