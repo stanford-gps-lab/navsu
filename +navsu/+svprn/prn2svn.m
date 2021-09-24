57e2c6d4552271aa7386e2e4b290a968be8503cf
Fabian <fabianr@stanford.edu>
2021-09-24 16:28:50 -0500

    enabled prn2svn to be called with input vectors

--- +navsu/+svprn/prn2svn.m
+++ +navsu/+svprn/prn2svn.m
@@ -17,17 +17,17 @@ if nargin < 2
     error('You must specify an svn and a time');
 end
 
+if length(epoch) == 1
+    epoch = epoch*ones(size(prn));
+end
+
 % Constellations
 % GPS = 1, GLO = 2, GAL = 3, BDS = 4
 if nargin < 3
     % Default to GPS
-    const =  1;
-end
-
-
-
-% Check for string constellation type input and convert to number
-if ischar(const)
+    const = ones(size(prn));
+elseif ischar(const)
+    % Check for string constellation type input and convert to number
     constNames = {'GPS','GLO','GAL','BDS','SBAS','QZSS'};
     const = find(~cellfun(@isempty,(strfind(constNames,const))));
     if isempty(const)
@@ -36,57 +36,51 @@ if ischar(const)
     end
 end
 
-if nargin < 4
-    % Source of database (only applies to GLONASS)
-    if const == 2
-        source = 3;
-    else
-        source = 1;
-    end
+if numel(const) == 1
+    const = const * ones(size(prn));
 end
 
-if any(epoch < 3000)
-    sdx = epoch < 3000;
+if nargin < 4
+    % Source of database (only applies to GLONASS)
+    source = ones(size(epoch));
+    source(const == 2) = 3;
+elseif numel(source) == 1
+    source = source * ones(size(epoch));
+end
+
+sdx = epoch < 3000;
+if any(sdx)
     year = floor(epoch(sdx));
     dayn = ceil(navsu.time.YearDays(year)*(epoch(sdx) - year));
     epoch(sdx) = navsu.time.doy2jd(year, dayn);
 end
-if length(epoch) == 1
-    epoch = epoch*ones(size(prn));
-end
 
-% Pull svndata table
-svndata = navsu.svprn.constSvnData(const,source);
-
-%find start date in jd
-svndata(:,end+1) = navsu.time.cal2jd_vect(svndata(:,3), svndata(:,4), ...
-    svndata(:,5)) + (svndata(:,6) + svndata(:,7)/60)/24;
-%find end date in jd
-svndata(:,end+1) = navsu.time.cal2jd_vect(svndata(:,8), svndata(:,9), ...
-    svndata(:,10) + (svndata(:,11) + svndata(:,12)/60)/24);
-% fix infinities
-svndata(svndata(:,8) == Inf,end) = Inf;
 
 svn = NaN(size(prn));
 
-% for pdx = 1:size(svndata,1)
-%     sdx = svndata(pdx,2) == prn & ...
-%            epoch >= svndata(pdx,end-1) & epoch <= svndata(pdx,end);
-%     if any(sdx)
-%         svn(sdx) = svndata(pdx,1);
-%         svn(sdx) = length(pdx);
-%     end
-% end
 
 for idx = 1:length(prn)
-    
+
+    % Pull svndata table
+    svndata = navsu.svprn.constSvnData(const(idx),source(idx));
+
+    %find start date in jd
+    epStart = navsu.time.cal2jd_vect(svndata(:,3), svndata(:,4), ...
+        svndata(:,5) + (svndata(:,6) + svndata(:,7)/60)/24);
+    %find end date in jd
+    epEnd = navsu.time.cal2jd_vect(svndata(:,8), svndata(:,9), ...
+        svndata(:,10) + (svndata(:,11) + svndata(:,12)/60)/24);
+    % fix infinities
+    epEnd(svndata(:,8) == Inf) = Inf;
+
     prni = prn(idx);
     epochi = epoch(idx);
-    sdx = find(svndata(:,2) == prni & epochi >= svndata(:,end-1) & epochi < svndata(:,end));
-    if ~isempty(sdx)
-        svn(idx) = svndata(sdx,1);
-    else
-        svn(idx) = NaN;
+    sdx = prni == svndata(:,2) ...
+        & epochi >= epStart ...
+        & epochi < epEnd;
+    
+    if any(sdx)
+        svn(idx) = svndata(sdx, 1);
     end
 end
 
