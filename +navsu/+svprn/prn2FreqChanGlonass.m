55feaf3e31310a0f554e10c820b57d00129849a5
Fabian Rothmaier <fabianr@stanford.edu>
2021-11-11 18:25:47 -0500

    updated svprn.prn2... functions to leverage common code. Added robustness to input dimensions.

--- +navsu/+svprn/prn2FreqChanGlonass.m
+++ +navsu/+svprn/prn2FreqChanGlonass.m
@@ -21,40 +21,7 @@ if nargin < 3
     source = 3;
 end
 
-if any(epoch < 3000)
-    sdx = epoch < 3000;
-    year = floor(epoch(sdx));
-    dayn = ceil(navsu.time.yearDays(year)*(epoch(sdx) - year));
-    epoch(sdx) = navsu.time.doy2jd(year, dayn);
-end
-if length(epoch) == 1
-    epoch = epoch*ones(size(prn));
-end
-
-% Pull svndata table 
-svndata = navsu.svprn.constSvnData(2,source);
-
-%find start date in jd       
-svndata(:,end+1) = navsu.time.cal2jd_vect(svndata(:,3), svndata(:,4), ...
-                    svndata(:,5)) + (svndata(:,6) + svndata(:,7)/60)/24;
-%find end date in jd       
-svndata(:,end+1) = navsu.time.cal2jd_vect(svndata(:,8), svndata(:,9), ...
-                    svndata(:,10) + (svndata(:,11) + svndata(:,12)/60)/24);       
-% fix infinities
-svndata(svndata(:,8) == Inf,end) = Inf;
-                
-freqChan = NaN(size(prn));       
-
-
-for idx = 1:length(prn)
-    prni = prn(idx);
-    epochi = epoch(idx);
-    sdx = find(svndata(:,2) == prni & epochi >= svndata(:,end-1) & epochi < svndata(:,end));
-    if ~isempty(sdx)
-        freqChan(idx) = svndata(sdx,14);
-    else
-        freqChan(idx) = NaN;
-    end
-end
+% call prn2x function for GLONASS constellation
+freqChan = navsu.svprn.prn2x(14, prn, epoch, 2*ones(size(prn)), source);
 
 end
--- +navsu/+svprn/prn2svn.m
+++ +navsu/+svprn/prn2svn.m
@@ -1,97 +1,20 @@
-function svn = prn2svn(prn, epoch, const, source)
-% prn2svn
+function svn = prn2svn(varargin)
+% prn2svn(prn, epoch, const, source)
 % DESCRIPTION:
 %   Map from PRN to SVN at a given time.
 %
 % INPUT:
-%   prn     - PRN
+%   prn     - N x 1 vector of PRNs
 %   epoch   - time is in fractional year (e.g 2012.233 very crudely) or 
 %             Julian day (e.g. 2444239.5)
+%   const   - N x 1 vector of constellation indices
+%   source  - OPTIONAL data source (only applies to GLONASS)
 %   
 % OUTPUT:
 %   svn     - satellite vehicle number.
 %
-% See also: navsu.svprn.svn2prn, navsu.time.cal2jd
+% See also: navsu.svprn.prn2x, navsu.svprn.svn2prn, navsu.time.cal2jd
 
-if nargin < 2
-    error('You must specify an svn and a time');
-end
-
-if length(epoch) == 1
-    epoch = epoch*ones(size(prn));
-end
-
-% Constellations
-% GPS = 1, GLO = 2, GAL = 3, BDS = 4
-if nargin < 3
-    % Default to GPS
-    const = ones(size(prn));
-elseif ischar(const)
-    % Check for string constellation type input and convert to number
-    constNames = {'GPS','GLO','GAL','BDS','SBAS','QZSS'};
-    const = find(~cellfun(@isempty,(strfind(constNames,const))));
-    if isempty(const)
-        warning('Invalid constellation name- defaulting to GPS')
-        const = 1;
-    end
-end
-
-if numel(const) == 1
-    const = const * ones(size(prn));
-end
-
-if nargin < 4
-    % Source of database (only applies to GLONASS)
-    source = ones(size(epoch));
-    source(const == 2) = 3;
-elseif numel(source) == 1
-    source = source * ones(size(epoch));
-end
-
-sdx = epoch < 3000;
-if any(sdx)
-    year = floor(epoch(sdx));
-    dayn = ceil(navsu.time.YearDays(year)*(epoch(sdx) - year));
-    epoch(sdx) = navsu.time.doy2jd(year, dayn);
-end
-
-
-svn = NaN(size(prn));
-
-% get constellation - source combinations to consider
-unCS = unique([const, source], 'rows');
-
-for csdx = 1:size(unCS, 1)
-    % retrieve constellation and source
-    consti = unCS(csdx, 1);
-    sourcei = unCS(csdx, 2);
-    
-    % Pull corresponding svndata table
-    svndata = navsu.svprn.constSvnData(consti, sourcei);
-    
-    % find start date in jd
-    epStart = navsu.time.cal2jd_vect(svndata(:,3), svndata(:,4), ...
-        svndata(:,5)) + (svndata(:,6) + svndata(:,7)/60)/24;
-    % find end date in jd
-    epEnd = navsu.time.cal2jd_vect(svndata(:,8), svndata(:,9), ...
-        svndata(:,10)) + (svndata(:,11) + svndata(:,12)/60)/24;
-    % fix infinities
-    epEnd(svndata(:,8) == Inf) = Inf;
-    
-    % save svn for satellites that are part of this case
-    
-    idx = find(const == consti & source == sourcei);
-    prni = prn(idx)';
-    epochi = epoch(idx)';
-    sdx = prni == svndata(:,2) ...
-        & epochi >= epStart ...
-        & epochi < epEnd;
-    % which PRNs do I have data for?
-    haveData = any(sdx, 1);
-    % get indices in SVN table, store SVN numbers
-    sdxInd = find(sdx) - size(svndata, 1)*(find(haveData)'-1);
-    svn(idx(haveData)) = svndata(sdxInd, 1);
-    
-end
+svn = navsu.svprn.prn2x(1, varargin{:});
 
 end
