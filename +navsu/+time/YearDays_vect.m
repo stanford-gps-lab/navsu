a1302118d312191f8def446f3224efce58caef87
toddwalter <twalter@stanford.edu>
2022-02-02 08:25:33 -0800

    Create YearDays_vect.m
    vectorized function that returns the number of days in a year

---
+++ +navsu/+time/YearDays_vect.m
@@ -0,0 +1,22 @@
+function nDays = YearDays_vect(year)
+% yearDays
+% DESCRIPTION:
+%   Compute the number of days in a given year accounting for leap years
+% INPUT:
+%   year = the year 
+%
+% OUTPUT:
+%   nDays = number of days in the year 
+%
+% See also: monthly calendars, daily calendars with jokes or comics
+
+% In the Gregorian calendar 3 criteria must be taken into account to identify leap years:
+% The year is evenly divisible by 4;
+% If the year can be evenly divided by 100, it is NOT a leap year, unless;
+% The year is also evenly divisible by 400. Then it is a leap year.
+
+nDays = repmat(365,size(year));
+
+nDays(mod(year,4) == 0 & ~(mod(year,100) == 0) | mod(year,400) == 0) = 366;
+
+end
