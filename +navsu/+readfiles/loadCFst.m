a094367e1726f7e4e272713601280986e7419528
rebeccaw <wrebecca@stanford.edu>
2022-08-24 18:24:31 -0700

    syntax fix in reading precise ephemeris and clock file

--- +navsu/+readfiles/loadCFst.m
+++ +navsu/+readfiles/loadCFst.m
@@ -40,7 +40,7 @@ function [Clck, CFileName,CFileNameFull] = loadCFst(Year, dayNum, settings, FLAG
 % Adjust in case day number is 0
 if dayNum == 0
     Year = Year-1;
-    dayNum = navsu.time.yearDays(Year);
+    dayNum = navsu.time.YearDays(Year);
 end
 
 if nargin < 4
